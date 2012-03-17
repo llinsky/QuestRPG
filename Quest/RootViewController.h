bc9a7dd99482023d6d4a64ca4a654291ee0359ab
Leo Linsky <LEO@leo-linskys-macbook.local>
2012-03-17 00:56:02 -0700

    Initial Commit

---
+++ Quest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Quest
+//
+//  Created by Leo Linsky on 3/17/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
